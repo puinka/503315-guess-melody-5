7b8a6c73bc92155cb74e274c71cd0e557de1d115
Valeria <vozzheva@mail.ru>
2020-10-25 12:13:22 +0000

    Добавляет HOC

---
+++ src/hocs/with-audio-player/with-audio-player.jsx
@@ -0,0 +1,37 @@
+import React, {PureComponent} from "react";
+import AudioPlyer from "../../components/audio-player/audio-player";
+
+const withActivePlayer = (Component) => {
+  class WithActivePlayer extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        activePlayerId: 0
+      };
+    }
+
+    render() {
+      const {activePlayerId} = this.state;
+
+      return <Component
+        {...this.props}
+        renderPlayer={(src, id) => {
+          return (
+            <AudioPlyer
+              src={src}
+              isPlaying={id === activePlayerId}
+              onPlayButtonClick={() => this.setState({
+                activePlayerId: activePlayerId === id ? -1 : id
+              })} />
+          );
+        }} />;
+    }
+  }
+
+  withActivePlayer.propTypes = {};
+
+  return withActivePlayer;
+};
+
+export default withActivePlayer;
